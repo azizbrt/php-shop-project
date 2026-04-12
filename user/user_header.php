9876a909d7b54008aea29f34eef20c2f98e9def0
Mohamed AzizBarrouta <azizbarrouta56@gmail.com>
2026-04-12 22:59:47 +0100

    make a clean home for the user

--- user/user_header.php
+++ user/user_header.php
@@ -1,3 +1,15 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
 <header class="header">
     <div class="header-1">
         <div class="flex">
